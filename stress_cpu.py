30c11667612a177f7934972a51523c4b46983b4f
Telles Nóbrega <tellesmvn@lsd.ufcg.edu.br>
2014-11-21 15:58:23 -0300

    Update stress_cpu.py

--- stress_cpu.py
+++ stress_cpu.py
@@ -16,40 +16,86 @@ def get_mem_usage(total_mem, line):
 
 def get_io_usage(line):
     return(int(_split_line(line)[4]))
-
+    
 def main(args):
-    cpu_file_path = str(args[0])
-    mem_file_path = str(args[1])
-    io_file_path = str(args[2])
-
-    cpu_file = open(cpu_file_path, 'r') 
-    mem_file = open(mem_file_path, 'r') 
-    io_file = open(io_file_path, 'r') 
-
-    total_mem = psutil.phymem_usage().total
-    cpu_lines = cpu_file.readlines()
-    mem_lines = mem_file.readlines()
-    io_lines = io_file.readlines()
-    for i in range(1,len(cpu_lines)):
-        cpu_line = cpu_lines[i]
-        mem_line = mem_lines[i]
-        io_line = io_lines[i]
-
-        cpu_usage = get_cpu_usage(cpu_line)
-        mem_usage = get_mem_usage(total_mem, mem_line)
-        io_usage = get_io_usage(io_line)
-
-        print(cpu_usage)
-        print(mem_usage)
-        print(io_line)
-  
-        cmd = "lookbusy -c %i -m %i -d %i" % (cpu_usage, mem_usage, io_usage)
+    testing = str(args[0])
+    if testing == 'all':
+        cpu_file_path = str(args[1])
+        mem_file_path = str(args[2])
+        io_file_path = str(args[3])
         
-        os.system(cmd)
+        cpu_file = open(cpu_file_path, 'r') 
+        mem_file = open(mem_file_path, 'r') 
+        io_file = open(io_file_path, 'r') 
+        total_mem = psutil.phymem_usage().total
         
-        time.sleep(60)
+        cpu_lines = cpu_file.readlines()
+        mem_lines = mem_file.readlines()
+        io_lines = io_file.readlines()
         
-         
+        for i in range(1,len(cpu_lines)):
+            cpu_line = cpu_lines[i]
+            mem_line = mem_lines[i]
+            io_line = io_lines[i]
+    
+            cpu_usage = get_cpu_usage(cpu_line)
+            mem_usage = get_mem_usage(total_mem, mem_line)
+            io_usage = get_io_usage(io_line)
+    
+            #All together  
+            cmd = "lookbusy -c %i -m %i -d %i" % (cpu_usage, mem_usage, io_usage)
+            
+            
+            os.system(cmd)
+            
+            time.sleep(60)
+            
+    else if testing == 'cpu':
+        cpu_file_path = str(args[1])
+        cpu_file = open(cpu_file_path, 'r') 
+        cpu_lines = cpu_file.readlines()
+        
+        for i in range(1,len(cpu_lines)):
+            cpu_line = cpu_lines[i]
+           
+            cpu_usage = get_cpu_usage(cpu_line)
+            
+            cmd = "lookbusy -c %i" % (cpu_usage)
+            
+            os.system(cmd)
+            
+            time.sleep(60)
+            
+    else if testing == 'mem':
+        mem_file_path = str(args[1])
+        mem_file = open(mem_file_path, 'r') 
+        total_mem = psutil.phymem_usage().total
+        mem_lines = mem_file.readlines()
+
+        for i in range(1,len(cpu_lines)):
+            mem_line = mem_lines[i]
+            mem_usage = get_mem_usage(total_mem, mem_line)
+            
+            cmd = "lookbusy -c %i -m %i -d %i" % (cpu_usage, mem_usage, io_usage)
+                
+            os.system(cmd)
+            
+            time.sleep(60)
+            
+    else if testing == 'io':
+        io_file_path = str(args[1])
+        io_file = open(io_file_path, 'r') 
+        io_lines = io_file.readlines()
+        for i in range(1,len(io_lines)):
+            io_line = io_lines[i]
+    
+            io_usage = get_io_usage(io_line)
+    
+            cmd = "lookbusy -d %i" % (io_usage)
+            
+            os.system(cmd)
+            
+            time.sleep(60)
 
 if __name__ == "__main__":
   main(sys.argv[1:])                                                             
